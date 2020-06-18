acded2fc5554b3909efae2b76a7d745ffbb4b6da
Andrew Moore <Andrew.Moore@navihealth.com>
2020-06-18 17:53:57 -0500

    tic tac toe

--- main.py
+++ main.py
@@ -33,13 +33,52 @@ def two():
     print(fibonacciSeries)
     print(sum)
 
-def main():
-    bigNum = 600851475143
-    factors = []
-    
-    
+def three():
+    n = 600851475143
+    primeFactors = []
+    i = 2
 
-    print(factors)
+    while n > 1:
+        if n % i == 0:
+            factors.append(i)
+            n //= i
+            i -= 1
+        i += 1
+    
+    print(primeFactors)
+
+def main():
+    board = [[0, 0, 0],
+             [0, 0, 0],
+             [0, 0, 0]]
+    print(board)
+
+    while True:
+        i = 0
+        while True:
+            if (i%2 != 0):
+                player = "O"
+            else:
+                player = "X"
+                
+            print("---------Player " + player + "--------------")
+            print("Enter Row Number: ")
+            row = int(input())
+            print("Enter Column Number: ")
+            col = int(input())
+
+            if (board[row][col] == 0):
+                if (player == "X"):
+                    board[row][col] = 1
+                else:
+                    board[row][col] = 2
+                i+=1
+            else:
+                print("This slot is taken fool!")
+
+
+            print(board)
+
 
         
         
